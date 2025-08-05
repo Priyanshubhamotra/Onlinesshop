f00563a9d21cf3ed2c5acb7851c99d98f08434e9
Priyanshubhamotra <165067249+Priyanshubhamotra@users.noreply.github.com>
2025-08-05 20:10:34 +0530

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -1,8 +1,8 @@
 <?php
 
-$db_name = 'if0_39638326_shop_db';
-$user_name = 'if0_39638326';
-$user_password = 'ojD4iacl6Uc';
+$db_name = 'b7_39639425_shop_db';
+$user_name = 'b7_39639425';
+$user_password = '8ballpool';
 
 $conn = new PDO($db_name, $user_name, $user_password);
 
