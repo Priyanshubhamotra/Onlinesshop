c08c17ab822cd6f40ef73b99e34af31c09655d68
Priyanshubhamotra <165067249+Priyanshubhamotra@users.noreply.github.com>
2025-08-05 20:16:32 +0530

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -1,9 +1,16 @@
 <?php
 
+$host = 'sql311.byethost7.com'; // or use the actual host name provided by your hosting provider
 $db_name = 'b7_39639425_shop_db';
 $user_name = 'b7_39639425';
 $user_password = '8ballpool';
 
-$conn = new PDO($db_name, $user_name, $user_password);
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db_name", $user_name, $user_password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 
 ?>
